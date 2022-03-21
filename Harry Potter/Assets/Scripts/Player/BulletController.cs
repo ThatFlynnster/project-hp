806456d77b3dc4c6dcb764910a45c078ed4589a4
ThatFlynnster <thatflynnster26@gmail.com>
2022-03-21 14:03:33 +0900

    base stats for MC
    utilized scriptable objects to grant unique stats for each character

--- Harry Potter/Assets/Scripts/Player/BulletController.cs
+++ Harry Potter/Assets/Scripts/Player/BulletController.cs
@@ -4,22 +4,34 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
+    public BaseStats stats;
+
     [SerializeField]
     private GameObject attackDecal;
 
-    private float speed = 100f;
-    private float lifeTime = 5f;
+    private float speed;
+    private AnimationCurve speedCurve;
+    private float maxLifeTime = 5f;
+    private float lifeTime = 0f;
 
     public Vector3 target { get; set; }
     public bool hit { get; set; }
 
     private void OnEnable()
     {
-        Destroy(gameObject, lifeTime);
+        Destroy(gameObject, maxLifeTime);
+    }
+
+    void Start()
+    {
+        speedCurve = stats.atkVelocity;
     }
 
     void Update()
     {
+        lifeTime += Time.deltaTime;
+        speed = speedCurve.Evaluate(lifeTime);
+
         transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
         if (!hit && Vector3.Distance(transform.position, target) < 0.005f)
         {
