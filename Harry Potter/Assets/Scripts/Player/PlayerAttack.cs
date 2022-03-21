8ffa1db2064270b2d339b4ac61e23f26d8962e2e
ThatFlynnster <thatflynnster26@gmail.com>
2022-03-21 19:04:22 +0900

    apparition test

--- Harry Potter/Assets/Scripts/Player/PlayerAttack.cs
+++ Harry Potter/Assets/Scripts/Player/PlayerAttack.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerAttack : MonoBehaviour
 {
-    
+    public BaseStats stats;
+
+    private InputAction attackAction;
 }
