97672eecbadeaf3d6f632e85a6820155ffcf2c2e
ThatFlynnster <101970558+ThatFlynnster@users.noreply.github.com>
2022-05-18 14:08:00 +0900

    Update BaseStats.cs

--- Harry Potter/Assets/Scripts/ScriptableObjects/Player/BaseStats.cs
+++ Harry Potter/Assets/Scripts/ScriptableObjects/Player/BaseStats.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Player Base Stats")]
 public class BaseStats : ScriptableObject
 {
-    public string chrName = "";
-    public int hp = 250;
+    public string chrName;
+    public int hp;
 
     public int atkDamage;
     public float atkSpeed;
     public float atkDuration;
     public AnimationCurve atkVelocity;
 
-    public int atkStamina;
-    public int defStamina;
+    public float atkStamina;
+    public float defStamina;
 
-    public int dashCount = 3;
+    public int meleeDamage;
+    public int dashCount;
+    public string passiveIdx;
 }
